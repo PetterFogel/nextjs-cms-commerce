cbd238f2c1ccf8065637ba8d03a52a179ab9ab1a
PetterFogel <petterfogel96@gmail.com>
2023-11-27 18:33:32 +0100

    Add new schemas

---
+++ sanity/schemas/product.ts
@@ -0,0 +1,36 @@
+export default {
+  name: 'product',
+  type: 'document',
+  title: 'Products',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name of Product',
+    },
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Description of product',
+    },
+    {
+      name: 'images',
+      type: 'array',
+      title: 'Product Images',
+      of: [{type: 'image'}],
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      options: {
+        source: 'name',
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    },
+  ],
+}
