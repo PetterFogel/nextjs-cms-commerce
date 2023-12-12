4ce46ff48c1ec098ae462ce51ce85cbe9d9a05bf
PetterFogel <petterfogel96@gmail.com>
2023-12-12 16:36:54 +0100

    Implement meta data to improve seo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,12 @@
-import PageSection from "@/components/page-section/PageSection";
+import { Metadata } from "next";
 import { getPageSection } from "@/lib/sanity.queries";
+import PageSection from "@/components/page-section/PageSection";
+
+export const metadata: Metadata = {
+  title: "About",
+  description:
+    "Born in the heart of Sweden, our clothing brand intertwines minimalist design and sustainable craftsmanship, echoing the beauty of Nordic simplicity."
+};
 
 const AboutPage = async () => {
   const sectionData = await getPageSection("About");
