f8815c319ae25a9f3fbc95be86b64128cf3f1e32
PetterFogel <petterfogel96@gmail.com>
2023-12-20 18:03:06 +0100

    Install and implement skeleton grid to handle loading

---
+++ app/(store)/[gender]/loading.tsx
@@ -0,0 +1,5 @@
+import SkeletonGrid from "@/components/skeleton-grid/SkeletonGrid";
+
+const Loading = () => <SkeletonGrid itemsLength={8} />;
+
+export default Loading;
---
+++ components/skeleton-grid/SkeletonGrid.tsx
@@ -0,0 +1,21 @@
+import { Skeleton } from "../ui/skeleton";
+
+interface Props {
+  itemsLength: number;
+}
+
+const SkeletonGrid = ({ itemsLength }: Props) => {
+  return (
+    <div className="mt-2 grid grid-cols-2 gap-x-4 gap-y-6 px-4 md:grid-cols-3 lg:grid-cols-4">
+      {Array(itemsLength)
+        .fill(0)
+        .map((_, idx) => (
+          <div key={idx}>
+            <Skeleton className="h-[200px] w-full sm:h-[300px] md:h-[350px] lg:h-[300px] xl:h-[450px]" />
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default SkeletonGrid;
